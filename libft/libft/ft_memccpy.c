870aa62799cc5d5640b4d85f83e7134ce5560246
ytkimirti <yusuftaha9@gmail.com>
2022-01-03 16:34:12 +0300

    autocommit

---
+++ libft/libft/ft_memccpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 15:43:09 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/01/03 15:48:54 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (i < (int)n)
+	{
+		*(char *)(dst + i) = *(char *)(src + i);
+		if (*(unsigned char *)(dst + i) == (unsigned char)c)
+		{
+			return (dst + i + 1);
+		}
+		i++;
+	}
+	return (NULL);
+}
