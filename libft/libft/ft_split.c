8ea68aabd53098a58480d4cb27f22cd34fe89c71
ytkimirti <yusuftaha9@gmail.com>
2022-01-04 18:16:42 +0300

    autocommit

---
+++ libft/libft/ft_split.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/04 17:56:04 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/01/04 18:14:43 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	strl(char *str, char key)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0 && str[i] != key)
+		i++;
+	return (i);
+}
+
+static int	find_str(char *str, char key)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	c = 0;
+	while (*str != 0)
+	{
+		while (*str == key && *str != 0)
+			str++;
+		if (*str == 0)
+			return (c);
+		c++;
+		while (*str != key && *str != 0)
+			str++;
+	}
+	return (c);
+}
+
+static char	*strcopy(char *dest, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (*src != 0 && i < n)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		i++;
+	}
+	*dest = 0;
+	return (dest);
+}
+
+char	**ft_split(char *str, char key)
+{
+	char	**str_m;
+	int		c;
+	int		i;
+
+	i = 0;
+	c = find_str(str, key);
+	str_m = (char **)malloc(sizeof(char *) * (c + 1));
+	if (str_m == NULL)
+		return (NULL);
+	while (i < c)
+	{
+		while (*str == key && *str != 0)
+			str++;
+		if (*str == 0)
+			return (str_m);
+		str_m[i] = (char *)malloc((strl(str, key) + 1) * sizeof(char));
+		if (str_m[i] == NULL)
+			return (NULL);
+		strcopy(str_m[i], str, strl(str, key));
+		while (*str != key && *str != 0)
+			str++;
+		i++;
+	}
+	return (str_m[c] = 0, str_m);
+}
--- libft/libft/ft_strtrim.c
+++ libft/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ykimirti <42istanbul.com.tr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 17:22:13 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/01/04 17:53:36 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/01/04 18:16:26 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "libft.h"
 #include <stdio.h>
 
-static bool	test_set(const char c,const char *set)
+static bool	test_set(const char c, const char *set)
 {
 	while (*set != 0)
 	{
@@ -31,34 +31,22 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char		*new_str;
 	int			i;
 
-	if (s1 == NULL || ft_strlen(s1) == 0)
+	if (ft_strlen(s1) == 0)
 		return (ft_strdup(""));
 	start = s1;
 	while (test_set(*start, set))
-	{
 		start++;
-	}
 	if (*start == '\0')
 		return (ft_strdup(""));
 	end = s1 + ft_strlen(s1) - 1;
 	while (test_set(*end, set))
-	{
-		/*printf("from end %c is in %s\n", *end, set);*/
 		end--;
-	}
-	/*printf("start: %c end: %c\n", *start, *end);*/
 	new_str = (char *)malloc((end - start + 2) * sizeof(char));
 	if (new_str == NULL)
 		return (NULL);
 	i = 0;
 	while (start <= end)
-	{
-		/*printf("i: %d of new_str is %c\n", i, *start);*/
-		new_str[i] = *start;
-		i++;
-		start++;
-	}
+		new_str[i++] = *start++;
 	new_str[i] = '\0';
-	/*printf("i: %d of new_str is 0\n", i);*/
 	return (new_str);
 }
