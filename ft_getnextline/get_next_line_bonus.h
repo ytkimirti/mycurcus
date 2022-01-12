ee43f50ce7b6a4af60ebe94666f8813d3fc0b049
ytkimirti <yusuftaha9@gmail.com>
2022-01-12 21:01:49 +0300

    ✅ getnextline %125

--- ft_getnextline/get_next_line_bonus.h
+++ ft_getnextline/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: ykimirti <42istanbul.com.tr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 12:32:59 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/01/12 14:19:21 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/01/12 21:01:03 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_BONUS_H
@@ -20,6 +20,7 @@ char	*ft_strjoin(const char *s1, const char *s2);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
 char	*ft_strdup(const char *s);
 
 #endif
