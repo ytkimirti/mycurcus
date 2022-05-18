bf4008f50821ce3edee3723bce263c5cde3a1327
Yusuf <yusuftaha9@gmail.com>
2022-05-18 16:11:01 +0300

    Autocommit

--- minitalk/client_bonus.c
+++ minitalk/client_bonus.c
@@ -1,4 +1,14 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/17 17:41:52 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/05/17 18:25:28 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h>
@@ -40,10 +50,21 @@ void send_str(int pid, char *str)
 	send_char(pid, '\0');
 }
 
+void receive(int sig, siginfo_t *info, void *ucontext)
+{
+	/*info->si_pid == jk*/
+}
+
 
 int main(int argc, char **argv)
 {
+	struct sigaction sg;
 	int pid;
+
+	sg = (struct sigaction){ 0 };
+	ft_printf("PID: %d\n", getpid());
+	sg.sa_sigaction = &receive;
+	sigaction(SIGUSR1, &sg, NULL);
 	if (argc != 3)
 	{
 		ft_printf("Usage:\nclient <pid> <message>");
